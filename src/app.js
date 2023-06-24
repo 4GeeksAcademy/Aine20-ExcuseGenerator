9fbbe3867f3e8847d1e46a781ef4cca049ea6b73
Josseth Sanchez <128737955+Aine20@users.noreply.github.com>
2023-06-24 00:29:38 +0000

    Changes in index.html and app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = Excuse();
+
+  document.querySelector("#btnNewExcuse").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = Excuse();
+  });
+};
+
+let Excuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let word1 = Math.floor(Math.random() * who.length);
+  let word2 = Math.floor(Math.random() * action.length);
+  let word3 = Math.floor(Math.random() * what.length);
+  let word4 = Math.floor(Math.random() * when.length);
+
+  return (
+    who[word1] + " " + action[word2] + " " + what[word3] + " " + when[word4]
+  );
 };
